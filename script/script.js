b0cd51496f5cdf1ff9e53d8686b6575786239d05
Biggie <ahmed_mikky@bloomfield.edu>
2023-11-21 11:59:47 -0500

    update

---
+++ script/script.js
@@ -0,0 +1,18 @@
+// scrollspy.js
+document.addEventListener('DOMContentLoaded', function () {
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const animationType = entry.target.getAttribute('data-animation');
+        entry.target.classList.add('animate__animated', `animate__${animationType}`, 'animate__delay-1s');
+      }
+    });
+  });
+
+  // Specify the elements to observe
+  const elementsToAnimate = document.querySelectorAll('.scrollspy');
+
+  elementsToAnimate.forEach(element => {
+    observer.observe(element);
+  });
+});

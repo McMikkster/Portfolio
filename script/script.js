221376ba3515bf3bed77986829aeff39cb7f4b56
Biggie <ahmed_mikky@bloomfield.edu>
2023-11-22 14:53:25 -0500

    update

--- script/script.js
+++ script/script.js
@@ -16,3 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
     observer.observe(element);
   });
 });
+
+date =  new Date();
+year = date.getFullYear()
+document.getElementById('date').innerHTML = "&copy; Ahmed Mikky " + year
